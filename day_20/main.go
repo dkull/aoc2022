dae69bd385a5d35f6a67153d6fffb8516e347384
Tanel Liiv <tanel.liiv@reconeyez.com>
2022-12-20 13:21:19 +0200

    day_20_p2

--- day_20/main.go
+++ day_20/main.go
@@ -11,6 +11,10 @@ import (
 Utils
 */
 
+type Num interface {
+	int | int64
+}
+
 func Fatal(err error) {
 	if err != nil {
 		panic(err)
@@ -24,130 +28,111 @@ func Abs(x int) int {
 	return x
 }
 
-func Modulus(a, b int) int {
+func Modulus[T Num](a, b T) T {
 	// return the modulus of a and b
-	modSteps := ((a % b) + b) % b
-	return modSteps
-	//return Abs((a + modSteps) % b)
+	return ((a % b) + b) % b
 }
 
 /*
 Structures
 */
 
-type SeqNum struct {
-	originalOffset int
-	value          int
+type SeqNum[T Num] struct {
+	originalOffset T
+	value          T
 }
 
-type RingBuffer struct {
-	insertIdxs int
-	numbers    []SeqNum
-	index      int
+type RingBuffer[T Num] struct {
+	insertIdxs T
+	numbers    []SeqNum[T]
+	index      T
 }
 
-func (rb *RingBuffer) Step(steps int) {
+func (rb *RingBuffer[T]) Step(steps T) {
 	/*correct and optimal handling of moving around the ring buffer
 	with both positive and negative numbers of any magnitude*/
-	//modSteps := (steps%len(rb.numbers) + steps) % len(rb.numbers)
-	//rb.index = (rb.index + modSteps) % len(rb.numbers)
-	rb.index = Modulus(steps+rb.index, len(rb.numbers))
+	rb.index = Modulus(steps+rb.index, T(len(rb.numbers)))
 }
 
-func (rb *RingBuffer) Add(num int) {
-	rb.numbers = append(rb.numbers, SeqNum{
-		originalOffset: rb.insertIdxs,
-		value:          num,
-	})
+func (rb *RingBuffer[T]) Add(num T) {
+	newSeqNum := new(SeqNum[T])
+	newSeqNum.originalOffset = rb.insertIdxs
+	newSeqNum.value = num
+	rb.numbers = append(rb.numbers, *newSeqNum)
 	rb.insertIdxs++
 }
 
-func (rb *RingBuffer) FindInsertIdx(originalOffset int) {
+func (rb *RingBuffer[T]) FindInsertIdx(originalOffset T) {
 	for rb.numbers[rb.index].originalOffset != originalOffset {
 		rb.Step(1)
 	}
 }
 
-func (rb *RingBuffer) FindValue(value int) {
+func (rb *RingBuffer[T]) FindValue(value T) {
 	for rb.numbers[rb.index].value != value {
 		rb.Step(1)
 	}
 }
 
-func (rb *RingBuffer) ReadValue() int {
+func (rb *RingBuffer[T]) ReadValue() T {
 	// read the value at the current index
 	return rb.numbers[rb.index].value
 }
 
-func (rb *RingBuffer) ShuffleValue() {
+func (rb *RingBuffer[T]) ShuffleValue() {
 	// get the value of the element at the current index
 	value := rb.numbers[rb.index].value
 	item := rb.numbers[rb.index]
-	//newLoc := Modulus(value, len(rb.numbers), &rb.index)
 
-	newLoc := Modulus(value+rb.index, len(rb.numbers)-1)
-	fmt.Println("> Shuffling", value, "to", newLoc, "from", rb.index, "while at", rb.index)
-	//rb.Step(value)
+	newLoc := Modulus(value+rb.index, T(len(rb.numbers)-1))
+	newSeqNums := new([]SeqNum[T])
 	if newLoc > rb.index {
-		newBuffer2 := [][]SeqNum{
-			rb.numbers[:rb.index], rb.numbers[rb.index+1 : newLoc+1], {item}, rb.numbers[newLoc+1:],
-		}
-		//fmt.Println("newBuffer2GT:", newBuffer2)
-		// concatenate the slices
-		resultBuffer := make([]SeqNum, 0, len(rb.numbers))
-		for _, slice := range newBuffer2 {
-			resultBuffer = append(resultBuffer, slice...)
-		}
-		rb.numbers = resultBuffer
+		*newSeqNums = append(*newSeqNums, rb.numbers[:rb.index]...)
+		*newSeqNums = append(*newSeqNums, rb.numbers[rb.index+1:newLoc+1]...)
+		*newSeqNums = append(*newSeqNums, item)
+		*newSeqNums = append(*newSeqNums, rb.numbers[newLoc+1:]...)
 	} else {
-		//fmt.Println("item", item, rb.index, newLoc)
-		newBuffer2 := [][]SeqNum{
-			rb.numbers[:newLoc], {item}, rb.numbers[newLoc:rb.index], rb.numbers[rb.index+1:],
-		}
-		//fmt.Println("newBuffer2LT:", newBuffer2)
-		// concatenate the slices
-		resultBuffer := make([]SeqNum, 0, len(rb.numbers))
-		for _, slice := range newBuffer2 {
-			resultBuffer = append(resultBuffer, slice...)
-		}
-		rb.numbers = resultBuffer
+		*newSeqNums = append(*newSeqNums, rb.numbers[:newLoc]...)
+		*newSeqNums = append(*newSeqNums, item)
+		*newSeqNums = append(*newSeqNums, rb.numbers[newLoc:rb.index]...)
+		*newSeqNums = append(*newSeqNums, rb.numbers[rb.index+1:]...)
 	}
+	rb.numbers = *newSeqNums
 }
 
 /*
 Functions
 */
 
-func part1(rb RingBuffer) int {
-	for elemIdx := 0; elemIdx < rb.insertIdxs; elemIdx++ {
-		// find the element in the RingBuffer
-		rb.FindInsertIdx(elemIdx)
-		/*fmt.Println("moving:", rb.numbers[rb.index].value)
-		fmt.Print("Before: ")
-		for _, elem := range rb.numbers {
-			fmt.Print(elem.value, " ")
+func run[T Num](rb RingBuffer[T], mixtimes int) T {
+	for i := 0; i < mixtimes; i++ {
+		for elemIdx := T(0); elemIdx < rb.insertIdxs; elemIdx++ {
+			// find the element in the RingBuffer
+			rb.FindInsertIdx(elemIdx)
+			/*fmt.Println("moving:", rb.numbers[rb.index].value)
+			fmt.Print("Before: ")
+			for _, elem := range rb.numbers {
+				fmt.Print(elem.value, " ")
+			}
+			fmt.Println()*/
+			rb.ShuffleValue()
+			/*fmt.Print(" After: ")
+			for _, elem := range rb.numbers {
+				fmt.Print(elem.value, " ")
+			}
+			fmt.Println()*/
 		}
-		fmt.Println()*/
-		rb.ShuffleValue()
-		/*fmt.Print(" After: ")
-		for _, elem := range rb.numbers {
-			fmt.Print(elem.value, " ")
-		}
-		fmt.Println()*/
 	}
 	// the answer
 	rb.FindValue(0)
-	sum := 0
-	for _, steps := range []int{1000, 1000, 1000} {
+	sum := T(0)
+	for _, steps := range []T{1000, 1000, 1000} {
 		rb.Step(steps)
 		sum += rb.ReadValue()
 	}
 	return sum
 }
 
-/*
-21725 is too high
-*/
 func main() {
 	// read in file using os.ReadFile from Argv[1]
 	data, err := os.ReadFile(os.Args[1])
@@ -157,14 +142,20 @@ func main() {
 	// trim the last empty line
 	lines = lines[:len(lines)-1]
 	// create a new RingBuffer
-	rb := RingBuffer{}
+	rb := RingBuffer[int64]{}
 	// loop through the lines
 	for _, line := range lines {
 		// parse the line as a number
 		num, err := strconv.Atoi(line)
 		Fatal(err)
 		// add the number to the RingBuffer
-		rb.Add(num)
+		rb.Add(int64(num))
 	}
-	fmt.Println("Part 1:", part1(rb))
+	fmt.Println("Part 1:", run(rb, 1))
+
+	for idx := range rb.numbers {
+		rb.numbers[idx].value = int64(rb.numbers[idx].value * 811589153)
+	}
+	fmt.Println("Part 2:", run(rb, 10))
+
 }
